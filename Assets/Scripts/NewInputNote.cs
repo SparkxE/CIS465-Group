464126b2fa815f138c964792eef032eb66c1bc20
Aaron Anderson <sylverblade16@gmail.com>
2023-09-09 21:26:26 -0500

    Added RPG Overworld movement basics, first commit

---
+++ Assets/Scripts/NewInputNote.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour{
+    //vars
+    protected Vector2 moveDir;
+    protected float accel;
+    [SerializeField] protected Rigidbody rb;
+
+    //methods
+    public void InMoveDir(InputAction.CallbackContext aCon){
+        moveDir = aCon.ReadValue<Vector2>();
+    }
+
+    protected void Move(){
+        rb.AddForce(moveDir, ForceMode.Force);
+    }
+
+    protected void FixedUpdate(){
+        Move();
+    }
+}
