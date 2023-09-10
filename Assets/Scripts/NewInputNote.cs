814cef671bb6dbb541999477c796b3379a247c44
Aaron Anderson <sylverblade16@gmail.com>
2023-09-09 21:30:09 -0500

    Commented out NewInputNote.cs since it was just meant to act as notes, was causing errors due to incomplete/improper setup of New Input System

--- Assets/Scripts/NewInputNote.cs
+++ Assets/Scripts/NewInputNote.cs
@@ -1,24 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour{
     //vars
-    protected Vector2 moveDir;
-    protected float accel;
-    [SerializeField] protected Rigidbody rb;
+    // protected Vector2 moveDir;
+    // protected float accel;
+    // [SerializeField] protected Rigidbody rb;
 
-    //methods
-    public void InMoveDir(InputAction.CallbackContext aCon){
-        moveDir = aCon.ReadValue<Vector2>();
-    }
+    // //methods
+    // public void InMoveDir(InputAction.CallbackContext aCon){
+    //     moveDir = aCon.ReadValue<Vector2>();
+    // }
 
-    protected void Move(){
-        rb.AddForce(moveDir, ForceMode.Force);
-    }
+    // protected void Move(){
+    //     rb.AddForce(moveDir, ForceMode.Force);
+    // }
 
-    protected void FixedUpdate(){
-        Move();
-    }
+    // protected void FixedUpdate(){
+    //     Move();
+    // }
 }
